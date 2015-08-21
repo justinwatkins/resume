e47f1f6d85910427a4f6b9faae9e0fd47838bd31
justin@24-7g.com <wt4846vc>
2015-08-21 14:12:02 -0500

    first commit

---
+++ resume/gulpfile.js
@@ -0,0 +1,48 @@
+﻿/// <binding BeforeBuild='bower' ProjectOpened='watcher' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+var project = require('./project.json'),
+        gulp = require('gulp'),
+        clean = require('gulp-clean'),
+        concat = require('gulp-concat'),
+        jshint = require('gulp-jshint'),
+        uglify = require('gulp-uglify'),
+        rename = require('gulp-rename'),
+        bower = require('gulp-bower'),
+        rimraf = require("rimraf"),
+        fs = require("fs"),
+        less = require("gulp-less"),
+        sass = require("gulp-sass");
+
+
+gulp.task("build", function () {
+    gulp.src('wwwroot/lib/*').pipe(clean());
+    bower();
+    gulp.src(['src/TypeScript/Tastes.js', 'src/TypeScript/Food.js'])
+                .pipe(concat("combined.js"))
+                .pipe(jshint())
+                .pipe(uglify())
+                .pipe(rename({
+                    extname: '.min.js'
+                }))
+                .pipe(gulp.dest('wwwroot/lib'));
+});
+
+gulp.task('resume', function () {
+    gulp.src('src/resume.html').pipe(gulp.dest(project.webroot + '/'));
+});
+
+gulp.task('sass', function () {
+    gulp.src(project.webroot + '/css/*').pipe(clean());
+    gulp.src('src/SCSS/resume.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(project.webroot + '/css'));
+});
+
+gulp.task("watcher", function () {
+    gulp.watch("src/TypeScript/*.js", ['build']);
+    gulp.watch("src/SCSS/*.scss", ['sass']);
+    gulp.watch("src/*.html", ['resume']);
+});
